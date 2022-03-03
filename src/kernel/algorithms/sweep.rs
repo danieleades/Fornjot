5c07363bd16f20c7adaa7705f9514e987e3aee5f
Hanno Braun <hanno@braun-odw.eu>
2022-03-03 16:33:07 +0100

    WIP Add task

--- src/kernel/algorithms/sweep.rs
+++ src/kernel/algorithms/sweep.rs
@@ -14,6 +14,9 @@ pub fn sweep_shape(
     path: Vector<3>,
     tolerance: Scalar,
 ) -> Shape {
+    // TASK: This could be called with 3-dimensional shapes, but it only works
+    //       for 2-dimensional ones.
+
     let mut shape = Shape::new();
 
     let translation = Transform::translation(path);
